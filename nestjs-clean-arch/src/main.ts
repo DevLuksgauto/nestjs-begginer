1e4807e8bc0fac3a1439a68b8456b3cfbbeca14b
Lucas Gauto <lukscgauto@gmail.com>
2025-01-30 23:52:04 -0300

    feat: config envirenment

--- nestjs-clean-arch/src/main.ts
+++ nestjs-clean-arch/src/main.ts
@@ -1,19 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    )
+    await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
-  .then(() => console.log('Application started successfully'))
-  .catch((err: Error) =>
-    console.error('Error starting application:', err.message),
-  );
+    .then(() => console.log('Application started successfully'))
+    .catch((err: Error) =>
+        console.error('Error starting application:', err.message),
+    )
