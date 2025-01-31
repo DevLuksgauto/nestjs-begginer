ef517f15119956648b833dcf157485a2bcbfe2b8
Lucas Gauto <lukscgauto@gmail.com>
2025-01-31 00:31:00 -0300

    refactor: refactoring env config module

--- nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.module.ts
+++ nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,30 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule, Module } from '@nestjs/common'
 import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
 @Module({
     providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule {
+    static forRoot<ValidationOptions extends Record<string, any>>(
+        options?: ConfigModuleOptions<ValidationOptions>,
+    ): DynamicModule {
+        return {
+            module: EnvConfigModule,
+            imports: [
+                ConfigModule.forRoot({
+                    ...options,
+                    envFilePath: [
+                        join(
+                            __dirname,
+                            `../../../../.env.${process.env.NODE_ENV}`,
+                        ),
+                    ],
+                }),
+            ],
+            providers: [EnvConfigService],
+            exports: [EnvConfigService],
+        }
+    }
+}
